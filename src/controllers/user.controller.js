248abe929a8afde2893ade68ca7eb5b2560792af
ishan nagar <inagar530@gmail.com>
2023-11-29 23:06:37 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {User} from "../models/user.model.js";
+import {User} from "../models/user.models.js";
 import {uploadOnCloudinary} from "../utils/Cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,26 +1,10 @@
-// import multer from "multer";
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {  // destination to save the files on diskstorage
-//       cb(null, "./public/temp")
-//     },
-//     filename: function (req, file, cb) {  // cb is callback function, and file accessd only multer
-      
-//       cb(null, file.originalname)
-//     }
-//   })
-  
-// export const upload = multer({ 
-//     storage, 
-// })
-
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function (req, file, cb) {  // destination to save the files on diskstorage
       cb(null, "./public/temp")
     },
-    filename: function (req, file, cb) {
+    filename: function (req, file, cb) {  // cb is callback function, and file accessd only multer
       
       cb(null, file.originalname)
     }
