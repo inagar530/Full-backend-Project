e4e1a95aa74f81d7147a92ce19850ee30d9d7152
ishan nagar <inagar530@gmail.com>
2023-11-30 12:52:43 +0530

    changes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,20 +1,3 @@
-// import multer from "multer";
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {  // destination to save the files on diskstorage
-//       cb(null, "./public/temp")
-//     },
-//     filename: function (req, file, cb) {  // cb is callback function, and file accessd only multer
-      
-//       cb(null, file.originalname)
-//     }
-//   })
-  
-// export const upload = multer({ 
-//     storage, 
-// })
-// export const
-
 import multer from "multer";
 
 const storage = multer.diskStorage({
